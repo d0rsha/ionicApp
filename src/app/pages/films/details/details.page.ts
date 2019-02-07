3bd3fd64b79bcf5704a5a30f16c82e7606a63977
Anders@Klumpisen <avrethem@gmail.com>
2019-02-07 17:52:20 +0100

    Tabsbar working, routerLink working

--- src/app/pages/films/details/details.page.ts
+++ src/app/pages/films/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  filmId = null;
+
+  // ActivatedRoute used to extract :id from URL 
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
