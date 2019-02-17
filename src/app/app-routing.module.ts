aba8c3338c8b83e3a98a061dc8635ac681dd25a8
Anders@Klumpisen <avrethem@gmail.com>
2019-02-17 23:45:05 +0100

    Continue firebase; Login interface started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './components/menu/menu.module#MenuPageModule' },
- /* { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '*', redirectTo: 'list', pathMatch: 'full' },
-  { path: 'pages', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
-  { path: 'list', loadChildren: './pages/memory-book/list/list.module#ListPageModule' },
-  { path: 'menu', loadChildren: './components/menu/menu.module#MenuPageModule' },*/
+  // { path: '', loadChildren: './components/menu/menu.module#MenuPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
